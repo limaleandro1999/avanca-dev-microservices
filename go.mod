6826660f948328a1f28c04f6839fadd1da32f4d9
Leandro Lima <leandro.lima@oowlish.com>
2020-11-03 23:51:26 -0300

    adding base files

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/limaleandro1999/avancadev-microservice-day1
+
+go 1.15
+
+require github.com/hashicorp/go-retryablehttp v0.6.7
---
+++ payment/payment.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type Result struct {
+	Status string
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":9091", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	coupon := r.PostFormValue("coupon")
+	ccNumber := r.PostFormValue("ccNumber")
+
+	resultCoupon, err := makeHttpCall("http://localhost:9092", coupon)
+	log.Println(resultCoupon)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonData := validateFields(resultCoupon, ccNumber)
+
+	fmt.Fprintf(w, string(jsonData))
+}
+
+func validateFields(resultCoupon Result, ccNumber string) []byte {
+	result := Result{Status: "declined"}
+
+	if ccNumber == "1" {
+		result.Status = "approved"
+	}
+
+	if resultCoupon.Status == "invalid" {
+		result.Status = "invalid coupon"
+	}
+
+	jsonData, err := json.Marshal(result)
+	if err != nil {
+		log.Fatal("Error processing JSON")
+	}
+
+	return jsonData
+}
+
+func makeHttpCall(urlMicroservice string, coupon string) (Result, error) {
+	values := url.Values{}
+	values.Add("coupon", coupon)
+
+	res, err := http.PostForm(urlMicroservice, values)
+	if err != nil {
+		return Result{}, errors.New("Servidor fora do ar!")
+	}
+
+	defer res.Body.Close()
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("Error processing result")
+	}
+
+	result := Result{}
+
+	json.Unmarshal(data, &result)
+
+	return result, nil
+}
